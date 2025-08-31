4114e39b231a0b74ebeb2318ae90452e6872c69b
webgenie1 <webgenie@gmail.com>
2025-08-30 22:31:39 -0400

    added the basic form handling and added Typewriter plug in.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-/*alert("Hello World"); */
+function generatePoem(event) {
+  event.preventDefault();
+
+  alert("Generating poem");
+
+  /*let poemElement = document.querySelector("#poem");*/
+  new Typewriter("#poem", {
+    strings: ["La tombe dit a la rose"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+  /* poemElement.innerHTML = "La tombe dit a la rose"; */
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
